9c10654b248ed671eb9bc39808fa4c6872d28e5c
goliathp <paras.ch699@gmail.com>
2021-09-05 19:43:28 +1000

    Settings modified for heroku

--- Zimzo/settings.py
+++ Zimzo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'add_business'
 LOGIN_URL = 'user_login'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
